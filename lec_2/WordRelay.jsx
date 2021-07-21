74b16ceefeba2388594769ccdaae0ac843d90a3c
Lee Taeyeop <lee.taeyeop@hotmail.com>
2021-07-21 16:54:11 +0900

    wordrelay with class

--- lec_2/WordRelay.jsx
+++ lec_2/WordRelay.jsx
@@ -3,10 +3,49 @@ const { Component } = React;
 
 class WordRelay extends Component {
   state = {
-    text: "Hello, webpack"
+    word: "신나",
+    value: "",
+    result: "",
   };
+  input;
+  onSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+      this.setState({
+        result: '딩동댕',
+        word: this.state.value,
+        value: '',
+      })
+      this.input.focus();
+    }
+    else {
+      this.setState({
+        reulst: "땡",
+        value: '',
+      })
+      this.input.focus();
+    }
+  }
+
+  onChangeInput = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
+  onRefInput = (c) => {
+    this.input = c;
+  }
+  
   render() {
-    return <h1>{this.state.text}</h1>
+    return (
+      <>
+        <div>{this.state.word}</div>
+        <form onSubmit={this.onSubmit}>
+          <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
+          <button>입력 !</button>
+        </form>
+        <div id="result">{this.state.result}</div>
+      </>
+    ) 
   }
 }
 
