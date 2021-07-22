e4bdfabf41f63bfd4935a8ee70b9ebe9ef3ad644
Lee Taeyeop <lee.taeyeop@hotmail.com>
2021-07-22 22:29:23 +0900

    test IIFE

--- lec_4/ResponseCheck.jsx
+++ lec_4/ResponseCheck.jsx
@@ -49,7 +49,18 @@ const ResponseCheck = () => {
       <div id="screen" className={state} onClick={onClickScreen}>
         {message}
       </div>
-      {renderAverage()}
+      {(() => {
+        if (result.length === 0) {
+          return null;
+        }
+        else {
+          return <>
+            <div>평균시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
+            <button onClick={onReset}>리셋</button>
+          </>
+        }
+      })()}
+      {/* {renderAverage()} */}
     </>
   )
 }
